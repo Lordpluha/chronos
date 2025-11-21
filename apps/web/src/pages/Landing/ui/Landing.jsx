36a430a658f5d329667a6b4df40a135854a5c720
andreo-kalashnello <kotikfop@gmail.com>
2025-11-21 13:18:07 +0200

    fix landing

--- apps/web/src/pages/Landing/ui/Landing.jsx
+++ apps/web/src/pages/Landing/ui/Landing.jsx
@@ -40,7 +40,7 @@ export const Landing = () => {
               Join to
             </h1>
             <h2 className="text-5xl font-bold mb-6">
-              Chronosom
+              Chronos
             </h2>
 
             <div className="mb-8">
@@ -54,7 +54,7 @@ export const Landing = () => {
               size="lg"
               className="px-8 py-6 text-lg"
             >
-              Lets Go
+              Let's Go
               <img src="/arrow-up.svg" alt="" />
             </Button>
           </div>
